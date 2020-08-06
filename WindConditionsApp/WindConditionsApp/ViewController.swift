7284707fc55a51a7ae2fe23bec386bd0efccdcbf
Jamil Nawaz <jamilnawaz88@gmail.com>
2020-08-06 17:10:59 +0100

    Added new project, deployment target iOS 12 and useful gitignore

---
+++ WindConditionsApp/WindConditionsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WindConditionsApp
+//
+//  Created by Jamil Nawaz on 06/08/2020.
+//  Copyright © 2020 Jamil Nawaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
