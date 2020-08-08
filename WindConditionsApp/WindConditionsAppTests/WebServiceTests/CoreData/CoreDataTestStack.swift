6b3bf78133e644f7ed396efe5435493cda5e9626
Jamil Nawaz <jamilnawaz88@gmail.com>
2020-08-08 17:26:07 +0100

    Added CoreDataTestStack implementation. This will allow easier testing of core data objects and methods.

---
+++ WindConditionsApp/WindConditionsAppTests/WebServiceTests/CoreData/CoreDataTestStack.swift
@@ -0,0 +1,28 @@
+//
+// Created by Jamil Nawaz on 08/08/2020.
+// Copyright (c) 2020 Jamil Nawaz. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import XCTest
+
+class CoreDataTestStack {
+    let persistentContainer: NSPersistentContainer
+
+    var mainContext: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+
+    init() {
+        persistentContainer = NSPersistentContainer(name: "Model")
+        let description = persistentContainer.persistentStoreDescriptions.first
+        description?.type = NSInMemoryStoreType
+
+        persistentContainer.loadPersistentStores { _, error in
+            guard error == nil else {
+                fatalError("was unable to load store \(error!)")
+            }
+        }
+    }
+}
