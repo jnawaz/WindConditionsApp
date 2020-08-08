5ee560b236bc7ecbb61639b5f521ca3ab8711a1e
Jamil Nawaz <jamilnawaz88@gmail.com>
2020-08-08 19:18:00 +0100

    Added City Core Data tests.

--- WindConditionsApp/WindConditionsAppTests/WebServiceTests/CoreData/CityTests.swift
+++ WindConditionsApp/WindConditionsAppTests/WebServiceTests/CoreData/CityTests.swift
@@ -15,13 +15,29 @@ class CityTests: XCTestCase {
 
     private let testContext = CoreDataTestStack().mainContext
 
-   func testCityCreation() {
-       let city = City(context: testContext)
+    func testNoCities() {
+        var cities: Int = -1
+        let citiesRequest = NSFetchRequest<NSManagedObject>(entityName: "City")
+        do {
+            cities = try testContext.fetch(citiesRequest).count
+        } catch {
+            XCTFail("Failed to fetch cities")
+        }
+        XCTAssertEqual(cities, 0)
+    }
 
-       do {
-           try testContext.save()
-       } catch {
-           XCTFail("Failed to create and save city object")
-       }
-   }
+    func testAddingCityCoordinates() {
+        let city = newCity()
+        let coordinates = Coordinates(context: testContext)
+        coordinates.longitude = 1.09
+        coordinates.latitude = 1.10
+        city.coordinates = coordinates
+
+        XCTAssertEqual(city.coordinates?.latitude, coordinates.latitude)
+        XCTAssertEqual(city.coordinates?.longitude, coordinates.longitude)
+    }
+
+    func newCity() -> City {
+        return City(context: testContext)
+    }
 }
